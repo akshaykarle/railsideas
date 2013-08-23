002d8cdc0cb0930fb411cb7345671382eac3a91a
Akshay Karle <akshay.a.karle@gmail.com>
2013-08-23 23:07:33 +0530

    Complete rails girls app. Basic flow

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 RailsIdeas::Application.routes.draw do
+  resources :ideas
+
+  root :to => redirect('/ideas')
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
